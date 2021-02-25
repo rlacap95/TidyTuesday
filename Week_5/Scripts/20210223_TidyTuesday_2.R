5b7f8d78ed54885b36838882afc2a1e0255b0d31
Roland <roland.lacap.270@my.csun.edu>
2021-02-25 00:18:25 -0800

    updated script and also added comments updated figure

--- Week_5/Scripts/20210223_TidyTuesday_2.R
+++ Week_5/Scripts/20210223_TidyTuesday_2.R
@@ -23,31 +23,32 @@ employedClean <- employed %>%
 glimpse(earnClean)
 glimpse(employedClean)
 
+#Employed Figure
 employedClean_race<- employedClean %>% 
-  mutate(race = case_when(race_gender == "Black or African American" ~ "Black",
-                          race_gender == "White" ~ "White")) %>% 
-  filter(race %in% c("Black", "White")) %>% 
-  select(race,employ_n,year) %>% 
-  ggplot(aes(x=employ_n,
+  mutate(race = case_when(race_gender == "Black or African American" ~ "Black", #changed data to say "Black"
+                          race_gender == "White" ~ "White")) %>%  #changed data to say "White"
+  filter(race %in% c("Black", "White")) %>% #filtered out race to only show Black and White
+  select(race,employ_n,year) %>% #selected columns for race, employment number, and year
+  ggplot(aes(x=employ_n, #created ggplot
              y=race,
              fill=race))+
-  geom_bar(stat = "identity")+
-  theme_classic()+
-  labs(x= "Number of Persons Employed",
+  geom_bar(stat = "identity")+ #created bar chart
+  theme_classic()+ #theme for the figure 
+  labs(x= "Number of Persons Employed", #labeled axis titles and figure
        title="Disparity in Employment by Race")+
-  theme(axis.title.y=element_blank(),
-        axis.title.x = element_text(vjust = -0.5),
-        plot.title=element_text(hjust=0.5,
+  theme(axis.title.y=element_blank(), #removed y-axis title
+        axis.title.x = element_text(vjust = -0.5), #adjusted the x-axis title to go lower
+        plot.title=element_text(hjust=0.5, #centered the figure title and bolded it
                                 face="bold"),
-        legend.title = element_blank())+
-  scale_fill_manual(values=ghibli_palette("PonyoMedium")[c(3,4)])+
-  scale_x_continuous(labels=scales::unit_format(unit="M",
-                                                scale = 1e-6))
+        legend.title = element_blank())+ #removed legend title
+  scale_fill_manual(values=ghibli_palette("PonyoMedium")[c(3,4)])+ #color scale 
+  scale_x_continuous(labels=scales::unit_format(unit="M", 
+                                                scale = 1e-6)) #changed x-axis title to millions
 
 employedClean_race
 
+#Earn Figure
 earnClean_race<- earnClean %>%
-  filter(sex !="Both Sexes") %>% 
   filter(race %in% c("Black or African American", "White")) %>%
   filter(year %in% c(2015,2020)) %>% 
   ggplot(aes(x=year,
@@ -72,11 +73,11 @@ earnClean_race<- earnClean %>%
                      breaks=c(2010,2012,2014,2016,2018,2020))+
   scale_y_continuous(labels=scales::dollar_format(),
                      position = "right")
-
+earnClean_race
 
 plot<-employedClean_race+earnClean_race + plot_annotation(tag_levels = 'A')+
   plot_layout(guides = "collect")&theme(legend.position = "bottom")+
-  ggsave(here("Week_5","Output","20210223_Employment_and_Earnings_Disparities"))
+  ggsave(here("Week_5","Output","20210223_Employment_and_Earnings_Disparities.png"))
 
 plot
 
