1ecea6f94e464c2afbe7b74c27ec6179f99ec078
Roland <roland.lacap.270@my.csun.edu>
2021-03-20 15:00:17 -0700

    update tidy tuesday

--- Week_7/20210314_TidyTuesday.R
+++ Week_7/20210314_TidyTuesday.R
@@ -1,6 +1,7 @@
 ### Tidy Tuesday Week 7
 ### Created By: Roland Lacap
-### Creadted on: 2021-03-12
+### Created on: 2021-03-12
+### Updated on: 2021-03-18
 
 ### Load Libraries
 library(tidyverse)
@@ -16,11 +17,22 @@ library(ggstream)
 ### Load Data
 movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
 
+moviesStarwars <- movies %>%
+  filter(title %in% c("Star Wars",
+                      "Star Wars: Episode V - The Empire Strikes Back",
+                      "Star Wars: Episode VI - Return of the Jedi",
+                      "Star Wars: Episode III - Revenge of the Sith",
+                      "Star Wars: Episode II - Attack of the Clones",
+                      "Star Wars: Episode I - The Phantom Menace")) %>% 
+  select(title, year, budget_2013, domgross_2013) 
 
-moviesFiltered <- movies %>% 
-  select(year, title, budget_2013,domgross_2013, intgross_2013, metascore, imdb_rating) %>% 
-  filter(complete.cases(.)) %>% 
-  arrange(desc(imdb_rating)) %>% 
-  filter(domgross_2013 != "#N/A") %>% 
-  as.numeric(unlist(3))
+newStarwars <- moviesStarwars %>% 
+  as.data.frame(sapply("domgross_2013", as.numeric))
 
+StarwarsPlot <- moviesStarwars %>% 
+  ggplot(aes(y = domgross_2013,
+             x = budget_2013,
+             fill = title))+
+  geom_bar(stat = "identity")
+
+StarwarsPlot
