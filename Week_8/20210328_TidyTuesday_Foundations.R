725c6d52fe802bafde844d9fe76f694000fbf3b2
Roland Lacap <rolandlacap@Ro-Tool.local>
2021-03-28 21:47:36 -0700

    Add New TidyTuesday Script

---
+++ Week_8/20210328_TidyTuesday_Foundations.R
@@ -0,0 +1,62 @@
+### Tidy Tuesday Week 9
+### Created by: Roland Lacap
+### Created on: 2021-03-28
+
+### Load Libraries
+library(tidyverse)
+library(here)
+library(tidytuesdayR)
+library(PNWColors)
+library(magick)
+
+### Get Data
+allShades <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-30/allShades.csv')
+
+### Wrangle this massive data set!
+
+shadeSep <- allShades %>% 
+  select(brand, name, lightness, hex) %>% 
+  mutate(shade = case_when(
+  lightness >= .75 ~ "Light",
+lightness > 0.45 & lightness < 0.75 ~ "Medium",
+lightness <= 0.45 ~ "Dark"
+  )) %>% 
+  group_by(brand) %>% 
+  count(shade) %>% 
+  filter(brand %in% c("Anastasia Beverly Hills",
+                      "bareMinerals",
+                      "Clinique",
+                      "CoverGirl",
+                      "Dior",
+                      "e.l.f. Cosmetics",
+                      "FENTY BEAUTY by Rihanna",
+                      "HUDA BEAUTY",
+                      "Lancôme",
+                      "MAC",
+                      "MAKE UP FOR EVER",
+                      "Maybelline",
+                      "NARS",
+                      "NYX Professional Makeup",
+                      "SEPHORA COLLECTION",
+                      "Shiseido",
+                      "Smashbox",
+                      "Tarte",
+                      "Too Faced",
+                      "Urban Decay Cosmetics",
+                      "Yves Saint Laurent",
+                      "ULTA",
+                      "Wet n Wild"))
+
+### Make a plot
+
+FoundationPlot <- shadeSep %>% 
+  ggplot(aes(x = shade,
+             y = n))+
+  geom_col()+
+  scale_x_discrete(limits = c("Light",
+                              "Medium",
+                              "Dark"))+
+  facet_wrap(~brand, scales = "free")
+
+FoundationPlot
+         
